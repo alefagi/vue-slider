4cfe08e7be83002fcfe10ec9627a997e144f888e
alefagi <iltu@libero.it>
2021-07-28 16:04:47 +0200

    added slider logic

--- js/script.js
+++ js/script.js
@@ -3,7 +3,26 @@ Vue.config.devtools = true;
 const app = new Vue({
   el: '#app',
   data: {
-    message: 'ciao',
+    currentIndex: 0,
+    images: ['images/image1.jpg', 'images/image2.jpg', 'images/image3.jpg', 'images/image4.jpg'],
   },
-  methods: {},
+  methods: {
+    isActive(index) {
+      return this.currentIndex === index ? 'active' : '';
+    },
+    toLeftIndex() {
+      if (this.currentIndex === 0) {
+        this.currentIndex = this.images.length - 1;
+      } else {
+        this.currentIndex--;
+      }
+    },
+    toRightIndex() {
+      if (this.currentIndex === this.images.length - 1) {
+        this.currentIndex = 0;
+      } else {
+        this.currentIndex++;
+      }
+    },
+  },
 });
